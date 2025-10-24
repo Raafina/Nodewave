fb8e99eff0d751f96b467ce40cf4aa45a62095d6
Raafina <raafinuradzani16@gmail.com>
2025-10-24 19:55:55 +0700

    feat: login and register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import AppShell from '@/components/commons/AppShell';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'sonner';
+import { onErrorHandler } from '@/libs/axios/response.handler';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false, // for not looping touch API
+      throwOnError(error) {
+        onErrorHandler(error as Error);
+        return false;
+      },
+    },
+    mutations: {
+      onError: onErrorHandler,
+    },
+  },
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <AppShell>
+          <Component {...pageProps} />
+          <Toaster richColors position="top-center" />
+        </AppShell>
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 }
